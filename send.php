ecb7af55544ae8b43af4cc4411e4682b4b4f4101
am-artsite <71486607+am-artsite@users.noreply.github.com>
2020-09-18 15:20:48 +0300

    php changes 1

--- send.php
+++ send.php
@@ -6,12 +6,12 @@ $message = trim($message);
 
 echo $message;
 
-mail("example@mail.ru", "Сообщение с сайта", "Нам пишут, что ".$message ,"From: example2@mail.ru \r\n");
+mail("to_am@rambler.ru", "Сообщение с сайта", "Нам пишут, что ".$message ,"From: info@manuylovich.ru \r\n");
 
 // Вместо example@mail.ru должен быть email адрес на который нужно отправить письмо,
 // а вместо example2@mail.ru должен быть существующий email данного сайта. 
 
-if (mail("example@mail.ru", "Сообщение с сайта", "Нам пишут, что ".$message ,"From: example2@mail.ru \r\n"))
+if (mail("to_am@rambler.ru", "Сообщение с сайта", "Нам пишут, что ".$message ,"From: info@manuylovich.ru \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
     echo "при отправке сообщения возникли ошибки";
